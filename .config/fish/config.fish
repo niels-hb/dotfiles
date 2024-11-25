5c2145b47f6782b4abaf9be00de12ff5626ee9b0
Niels Heidbrink <niels.heidbrink@proton.me>
2024-11-25 13:58:50 +0700

    Switched to fish from zsh, zellij from tmux and starship from oh-my-zsh

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+eval "$(devbox global shellenv)"
+
+if status is-interactive
+    export ZELLIJ_AUTO_ATTACH=true
+    export ZELLIJ_AUTO_EXIT=true
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
+
+set -U fish_greeting
+set -Ux EDITOR nvim
+
+alias vi=nvim
+alias vim=nvim
+
+function starship_transient_prompt_func
+  starship module character
+end
+starship init fish | source
+enable_transience
