6d29d06914075b884e05d81d75e69e3e36549007
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 12:08:52 +0700

    Show @recording messages in statusline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,13 @@ return {
     sections = {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {},
+      lualine_c = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        }
+      },
       lualine_x = {},
       lualine_y = {},
       lualine_z = { 'buffers' }
