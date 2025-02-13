240408d517000a750f0d8f213a4c629504a51980
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 12:08:52 +0700

    Enabled hidden files in telescope find_files picker

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,14 @@ return {
   tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup({
+      pickers = {
+        find_files = {
+          hidden = true
+        },
+      }
+    })
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
