457f8f2a27a94998bebe5ac4f1ba7a3250b52e13
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-03-01 15:50:46 +0700

    Updated nvim-conform: Added prettier formatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -21,6 +21,11 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { "goimports", "gofmt" },
+      astro = { "prettier" },
+      css = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      markdown = { "prettier" },
       ["*"] = { "trim_newlines", "trim_whitespace" }
     },
   },
