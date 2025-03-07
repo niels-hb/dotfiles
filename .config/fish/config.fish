cd9d3ab8a03f24d329c4f23e0544c0a39ae66545
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-03-07 16:53:30 +0700

    Enabled brew fish completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,12 @@ function starship_transient_prompt_func
 end
 starship init fish | source
 enable_transience
+
+# brew completions
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
