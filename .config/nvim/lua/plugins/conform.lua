f1133fecbbfa794646834a5a427e0291a030f446
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-04-21 15:39:05 +0700

    Upgraded conform to v9

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,32 +1,9 @@
 return {
   'stevearc/conform.nvim',
-  lazy = false,
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format({ async = true, lsp_fallback = true })
-      end,
-      mode = 'n',
-    },
-  },
   opts = {
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      return {
-        timeout_ms = 2000,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      go = { "goimports", "gofmt" },
-      astro = { "prettier" },
-      css = { "prettier" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      markdown = { "prettier" },
-      ["*"] = { "trim_newlines", "trim_whitespace" }
+    format_on_save = {
+      timeout_ms = 2000,
+      lsp_format = "fallback",
     },
   },
 }
