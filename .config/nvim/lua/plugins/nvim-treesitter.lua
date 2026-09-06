ce91863a2e4a5f128012f6adcd40ba189ce2a67e
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2026-09-06 09:35:41 -0300

    Updated nvim-treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,22 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate'
+  branch = 'main',
+  build = ':TSUpdate',
+  lazy = false,
+  config = function()
+    require('nvim-treesitter').install {
+      'bash',
+      'dart',
+      'fish',
+      'go',
+      'gomod',
+      'html',
+      'json',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'toml',
+      'yaml',
+    }
+  end,
 }
