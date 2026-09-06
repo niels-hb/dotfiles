855729ca3647e15b69a7a42c0ca053f506d4b73a
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2026-09-06 09:21:34 -0300

    Updated lazy.nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,20 +18,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.clipboard = 'unnamedplus'
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+require("config.lazy")
 
 vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>')
 
